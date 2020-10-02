06263c1db3a2f20553d5ee10e783d9806f5f0dfe
Javier <xleyba@users.noreply.github.com>
2020-10-02 19:38:14 +0200

    first commit

---
+++ eurekaserver/src/test/java/net/javier/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javier.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
